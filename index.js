bde3aa44eaf1d63ff0298a26287a39cbfa8b3145
toufik6815 <64719882+toufik6815@users.noreply.github.com>
2020-10-01 13:03:56 +0600

    port update

--- index.js
+++ index.js
@@ -22,10 +22,15 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
+
 const port = 5000;
 
 
 
+//ROOT
+app.get('/', (req, res) => {
+    res.send('Hello form db working')
+})
 
 
 
